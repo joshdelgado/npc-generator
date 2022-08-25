f6ff62bfd29bc1a3c6edf415b54992a7c6a448c3
Josh Delgado <josh.high5@gmail.com>
2022-08-25 14:44:13 -0600

    Added name, age, height, and weight generation

--- src/components/npc-card/npc-card.tsx
+++ src/components/npc-card/npc-card.tsx
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import { AbilityScores } from '../../interfaces/ability-scores';
 import { SimpleRange } from '../../interfaces/simple-range';
 import { Stat } from '../../interfaces/stat';
 
@@ -7,14 +6,78 @@ class RaceInfo {
 	age: SimpleRange = { min: 0, max: 0 };
 	height: SimpleRange = { min: 0, max: 0 };
 	weight: SimpleRange = { min: 0, max: 0 };
+	names: { male: string[], female: string[], surname: string[] } = { male: [], female: [], surname: [] };
 
-	constructor(minAge: number, maxAge: number, minHeight: number, maxHeight: number, minWeight: number, maxWeight: number) {
+	constructor(name: string, minAge: number, maxAge: number, minHeight: number, maxHeight: number, minWeight: number, maxWeight: number) {
+		this.setNames(name);
 		this.age.min = minAge;
 		this.age.max = maxAge;
 		this.height.min = minHeight;
 		this.height.max = maxHeight;
 		this.weight.min = minWeight;
 		this.weight.max = maxWeight;
+	}
+
+	setNames(name: string): void {
+		this.names.male = [];
+		this.names.female = [];
+		this.names.surname = [];
+		switch (name) {
+			case 'dragonborn':
+				this.names.male = ['Arjhan', 'Balasar', 'Bharash', 'Donaar', 'Ghesh', 'Heskan', 'Kriv', 'Medrash', 'Mehen', 'Nadarr', 'Pandjed', 'Patrin', 'Rhogar', 'Shamash', 'Shedinn', 'Tarhun', 'Torinn'];
+				this.names.female = ['Akra', 'Biri', 'Daar', 'Farideh', 'Harann', 'Havilar', 'Jheri', 'Kava', 'Korinn', 'Mishann', 'Nala', 'Perra', 'Raiann', 'Sora', 'Surina', 'Thava', 'Uadjit'];
+				this.names.surname = ['Clethtinthiallor', 'Daardendrian', 'Delmirev', 'Drachedandion', 'Fenkenkabradon', 'Kepeshkmolik', 'Kerrhylon', 'Kimbatuul', 'Linxakasendalor', 'Myastan', 'Nemmonis', 'Norixius', 'Ophinshtalajiir', 'Prexijandilin', 'Shestendeliath', 'Turnuroth', 'Verthisathurgiesh', 'Yarjerit'];
+				break;
+			case 'dwarf':
+				this.names.male = ['Adrik', 'Alberich', 'Baern', 'Barendd', 'Brottor', 'Bruenor', 'Dain', 'Darrak', 'Delg', 'Eberk', 'Einkil', 'Fargrim', 'Flint', 'Gardain', 'Harbek', 'Kildrak', 'Morgran', 'Orsik', 'Oskar', 'Rangrim', 'Rurik', 'Taklinn', 'Thoradin', 'Thorin', 'Tordek', 'Traubon', 'Travok', 'Ulfgar', 'Veit', 'Vondal'];
+				this.names.female = ['Amber', 'Artin', 'Audhild', 'Bardryn', 'Dagnal', 'Diesa', 'Eldeth', 'Falkrunn', 'Finellen', 'Gunnloda', 'Gurdis', 'Helja', 'Hlin', 'Kathra', 'Kristryd', 'Ilde', 'Liftrasa', 'Mardred', 'Riswynn', 'Sannl', 'Torbera', 'Torgga', 'Vistra'];
+				this.names.surname = ['Balderk', 'Battlehammer', 'Brawnanvil', 'Dankil', 'Fireforge', 'Frostbeard', 'Gorunn', 'Holderhek', 'Ironfist', 'Loderr', 'Lutgehr', 'Rumnaheim', 'Strakeln', 'Torunn', 'Ungart'];
+				break;
+			case 'elf':
+				this.names.male = ['Adran', 'Aelar', 'Aramil', 'Arannis', 'Aust', 'Beiro', 'Berrian', 'Carric', 'Enialis', 'Erdan', 'Erevan', 'Galinndan', 'Hadarai', 'Heian', 'Himo', 'Immeral', 'Ivellios', 'Laucian', 'Mindartis', 'Paelias', 'Peren', 'Quarion', 'Riardon', 'Rolen', 'Soveliss', 'Thamior', 'Tharivol', 'Theren', 'Varis', 'Aseir', 'Bardeid', 'Haseid', 'Khemed', 'Mehmen', 'Sudeiman', 'Zasheir', 'Darvin', 'Dorn', 'Evendur', 'Gorstag', 'Grim', 'Helm', 'Malark', 'Morn', 'Randal', 'Stedd', 'Bor', 'Fodel', 'Glar', 'Grigor', 'Igan', 'Ivor', 'Kosef', 'Mival', 'Orel', 'Pavel', 'Sergor', 'Ander', 'Blath', 'Bran', 'Frath', 'Geth', 'Lander', 'Luth', 'Malcer', 'Stor', 'Taman', 'Urth Aoth', 'Bareris', 'Ehput-Ki', 'Kethoth', 'Mumed', 'Ramas', 'So-Kehur', 'Thazar-De', 'Urhur', 'Borivik', 'Faurgar', 'Jandar', 'Kanithar', 'Madislak', 'Ralmevik', 'Shaumar', 'Vladislak', 'An', 'Chen', 'Chi', 'Fai', 'Jiang', 'Jun', 'Lian', 'Long', 'Meng', 'On', 'Shan', 'Shui', 'Wen', 'Anton', 'Diero', 'Marcon', 'Pieron', 'Rimardo', 'Romero', 'Salazar', 'Umbero'];
+				this.names.female = ['Adrie', 'Althaea', 'Anastrianna', 'Andraste', 'Antinua', 'Bethrynna', 'Birel', 'Caelynn', 'Drusilia', 'Enna', 'Felosial', 'Ielenia', 'Jelenneth', 'Keyleth', 'Leshanna', 'Lia', 'Meriele', 'Mialee', 'Naivara', 'Quelenna', 'Quillathe', 'Sariel', 'Shanairra', 'Shava', 'Silaqui', 'Theirastra', 'Thia', 'Vadania', 'Valanthe', 'Xanaphia'];
+				this.names.surname = ['Amakiir (Gemflower)', 'Amastacia (Starflower)', 'Galanodel (Moonwhisper)', 'Holimion (Diamonddew)', 'Ilphelkiir (Gemblossom)', 'Liadon (Silverfrond)', 'Meliamne (Oakenheel)', 'Naïlo (Nightbreeze)', 'Siannodel (Moonbrook)', 'Xiloscient (Goldpetal)'];
+				break;
+			case 'gnome':
+				this.names.male = ['Alston', 'Alvyn', 'Boddynock', 'Brocc', 'Burgell', 'Dimble', 'Eldon', 'Erky', 'Fonkin', 'Frug', 'Gerbo', 'Gimble', 'Glim', 'Jebeddo', 'Kellen', 'Namfoodle', 'Orryn', 'Roondar', 'Seebo', 'Sindri', 'Warryn', 'Wrenn', 'Zook'];
+				this.names.female = ['Bimpnottin', 'Breena', 'Caramip', 'Carlin', 'Donella', 'Duvamil', 'Ella', 'Ellyjobell', 'Ellywick', 'Lilli', 'Loopmottin', 'Lorilla', 'Mardnab', 'Nissa', 'Nyx', 'Oda', 'Orla', 'Roywyn', 'Shamil', 'Tana', 'Waywocket', 'Zanna'];
+				this.names.surname = ['Beren', 'Daergel', 'Folkor', 'Garrick', 'Nackle', 'Murnig', 'Ningel', 'Raulnor', 'Scheppen', 'Timbers', 'Turen'];
+				break;
+			case 'half-elf':
+				// combine elf and human
+				this.names.male = ['Adran', 'Aelar', 'Aramil', 'Arannis', 'Aust', 'Beiro', 'Berrian', 'Carric', 'Enialis', 'Erdan', 'Erevan', 'Galinndan', 'Hadarai', 'Heian', 'Himo', 'Immeral', 'Ivellios', 'Laucian', 'Mindartis', 'Paelias', 'Peren', 'Quarion', 'Riardon', 'Rolen', 'Soveliss', 'Thamior', 'Tharivol', 'Theren', 'Varis'];
+				this.names.female = ['Adrie', 'Althaea', 'Anastrianna', 'Andraste', 'Antinua', 'Bethrynna', 'Birel', 'Caelynn', 'Drusilia', 'Enna', 'Felosial', 'Ielenia', 'Jelenneth', 'Keyleth', 'Leshanna', 'Lia', 'Meriele', 'Mialee', 'Naivara', 'Quelenna', 'Quillathe', 'Sariel', 'Shanairra', 'Shava', 'Silaqui', 'Theirastra', 'Thia', 'Vadania', 'Valanthe', 'Xanaphia', 'Atala', 'Ceidil', 'Hama', 'Jasmal', 'Meilil', 'Seipora', 'Yasheira', 'Zasheida', 'Arveene', 'Esvele', 'Jhessail', 'Kerri', 'Lureene', 'Miri', 'Rowan', 'Shandri', 'Tessele', 'Alethra', 'Kara', 'Katernin', 'Mara', 'Natali', 'Olma', 'Tana', 'Zora', 'Amafrey', 'Betha', 'Cefrey', 'Kethra', 'Mara', 'Olga', 'Silifrey', 'Westra', 'Arizima', 'Chathi', 'Nephis', 'Nulara', 'Murithi', 'Sefris', 'Thola', 'Umara', 'Zolis', 'Fyevarra', 'Hulmarra', 'Immith', 'Imzel', 'Navarra', 'Shevarra', 'Tammith', 'Yuldra', 'Bai', 'Chao', 'Jia', 'Lei', 'Mei', 'Qiao', 'Shui', 'Tai', 'Balama', 'Dona', 'Faila', 'Jalana', 'Luisa', 'Marta', 'Quara', 'Selise', 'Vonda'];
+				this.names.surname = ['Amakiir (Gemflower)', 'Amastacia (Starflower)', 'Galanodel (Moonwhisper)', 'Holimion (Diamonddew)', 'Ilphelkiir (Gemblossom)', 'Liadon (Silverfrond)', 'Meliamne (Oakenheel)', 'Naïlo (Nightbreeze)', 'Siannodel (Moonbrook)', 'Xiloscient (Goldpetal)', 'Basha', 'Dumein', 'Jassan', 'Khalid', 'Mostana', 'Pashar', 'Rein', 'Amblecrown', 'Buckman', 'Dundragon', 'Evenwood', 'Greycastle', 'Tallstag', 'Bersk', 'Chernin', 'Dotsk', 'Kulenov', 'Marsk', 'Nemetsk', 'Shemov', 'Starag', 'Brightwood', 'Helder', 'Hornraven', 'Lackman', 'Stormwind', 'Windrivver', 'Ankhalab', 'Anskuld', 'Fezim', 'Hahpet', 'Nathandem', 'Sepret', 'Uuthrakt', 'Chergoba', 'Dyernina', 'Iltazyara', 'Murnyethara', 'Stayanoga', 'Ulmokina', 'Chien', 'Huang', 'Kao', 'Kung', 'Lao', 'Ling', 'Mei', 'Pin', 'Shin', 'Sum', 'Tan', 'Wan', 'Agosto', 'Astorio', 'Calabra', 'Domine', 'Falone', 'Marivaldi', 'Pisacar', 'Ramondo'];
+				break;
+			case 'half-orc':
+				// combine human and orc
+				this.names.male = ['Dench', 'Feng', 'Gell', 'Henk', 'Holg', 'Imsh', 'Keth', 'Krusk', 'Mhurren', 'Ront', 'Shump', 'Thokk'];
+				this.names.female = ['Baggi', 'Emen', 'Engong', 'Kansif', 'Myev', 'Neega', 'Ovak', 'Ownka', 'Shautha', 'Sutha', 'Vola', 'Volen', 'Yevelda'];
+				// no last names
+				this.names.surname = [''];
+				break;
+			case 'halfling':
+				this.names.male = ['Alton', 'Ander', 'Cade', 'Corrin', 'Eldon', 'Errich', 'Finnan', 'Garret', 'Lindal', 'Lyle', 'Merric', 'Milo', 'Osborn', 'Perrin', 'Reed', 'Roscoe', 'Wellby'];
+				this.names.female = ['Andry', 'Bree', 'Callie', 'Cora', 'Euphemia', 'Jillian', 'Kithri', 'Lavinia', 'Lidda', 'Merla', 'Nedda', 'Paela', 'Portia', 'Seraphina', 'Shaena', 'Trym', 'Vani', 'Verna'];
+				this.names.surname = ['Brushgather', 'Goodbarrel', 'Greenbottle', 'High-hill', 'Hilltopple', 'Leagallow', 'Tealeaf', 'Thorngage', 'Tosscobble', 'Underbough'];
+				break;
+			case 'human':
+				this.names.male = ['Aseir', 'Bardeid', 'Haseid', 'Khemed', 'Mehmen', 'Sudeiman', 'Zasheir', 'Darvin', 'Dorn', 'Evendur', 'Gorstag', 'Grim', 'Helm', 'Malark', 'Morn', 'Randal', 'Stedd', 'Bor', 'Fodel', 'Glar', 'Grigor', 'Igan', 'Ivor', 'Kosef', 'Mival', 'Orel', 'Pavel', 'Sergor', 'Ander', 'Blath', 'Bran', 'Frath', 'Geth', 'Lander', 'Luth', 'Malcer', 'Stor', 'Taman', 'Urth Aoth', 'Bareris', 'Ehput-Ki', 'Kethoth', 'Mumed', 'Ramas', 'So-Kehur', 'Thazar-De', 'Urhur', 'Borivik', 'Faurgar', 'Jandar', 'Kanithar', 'Madislak', 'Ralmevik', 'Shaumar', 'Vladislak', 'An', 'Chen', 'Chi', 'Fai', 'Jiang', 'Jun', 'Lian', 'Long', 'Meng', 'On', 'Shan', 'Shui', 'Wen', 'Anton', 'Diero', 'Marcon', 'Pieron', 'Rimardo', 'Romero', 'Salazar', 'Umbero'];
+				this.names.female = ['Atala', 'Ceidil', 'Hama', 'Jasmal', 'Meilil', 'Seipora', 'Yasheira', 'Zasheida', 'Arveene', 'Esvele', 'Jhessail', 'Kerri', 'Lureene', 'Miri', 'Rowan', 'Shandri', 'Tessele', 'Alethra', 'Kara', 'Katernin', 'Mara', 'Natali', 'Olma', 'Tana', 'Zora', 'Amafrey', 'Betha', 'Cefrey', 'Kethra', 'Mara', 'Olga', 'Silifrey', 'Westra', 'Arizima', 'Chathi', 'Nephis', 'Nulara', 'Murithi', 'Sefris', 'Thola', 'Umara', 'Zolis', 'Fyevarra', 'Hulmarra', 'Immith', 'Imzel', 'Navarra', 'Shevarra', 'Tammith', 'Yuldra', 'Bai', 'Chao', 'Jia', 'Lei', 'Mei', 'Qiao', 'Shui', 'Tai', 'Balama', 'Dona', 'Faila', 'Jalana', 'Luisa', 'Marta', 'Quara', 'Selise', 'Vonda'];
+				this.names.surname = ['Basha', 'Dumein', 'Jassan', 'Khalid', 'Mostana', 'Pashar', 'Rein', 'Amblecrown', 'Buckman', 'Dundragon', 'Evenwood', 'Greycastle', 'Tallstag', 'Bersk', 'Chernin', 'Dotsk', 'Kulenov', 'Marsk', 'Nemetsk', 'Shemov', 'Starag', 'Brightwood', 'Helder', 'Hornraven', 'Lackman', 'Stormwind', 'Windrivver', 'Ankhalab', 'Anskuld', 'Fezim', 'Hahpet', 'Nathandem', 'Sepret', 'Uuthrakt', 'Chergoba', 'Dyernina', 'Iltazyara', 'Murnyethara', 'Stayanoga', 'Ulmokina', 'Chien', 'Huang', 'Kao', 'Kung', 'Lao', 'Ling', 'Mei', 'Pin', 'Shin', 'Sum', 'Tan', 'Wan', 'Agosto', 'Astorio', 'Calabra', 'Domine', 'Falone', 'Marivaldi', 'Pisacar', 'Ramondo'];
+				break;
+			case 'tiefling':
+				this.names.male = ['Akmenos', 'Amnon', 'Barakas', 'Damakos', 'Ekemon', 'Iados', 'Kairon', 'Leucis', 'Melech', 'Mordai', 'Morthos', 'Pelaios', 'Skamos', 'Therai'];
+				this.names.female = ['Akta', 'Anakis', 'Bryseis', 'Criella', 'Damaia', 'Ea', 'Kallista', 'Lerissa', 'Makaria', 'Nemeia', 'Orianna', 'Phelaia', 'Rieta'];
+				// virtue names
+				this.names.surname = ['Art', 'Carrion', 'Chant', 'Creed', 'Despair', 'Excellence', 'Fear', 'Glory', 'Hope', 'Ideal', 'Music', 'Nowhere', 'Open', 'Poetry', 'Quest', 'Random', 'Reverence', 'Sorrow', 'Temerity', 'Torment', 'Weary'];
+				break;
+			default:
+				this.names.male = ['Josh', 'Mike', 'Pat', 'Justin', 'Hector The Well Endowed', 'Marrrrrr'];
+				this.names.female = ['Jess', 'Shannon', 'Mal', 'Riley'];
+				this.names.surname = ['Calderon', 'Batman', 'Graveler', 'Peepee', 'Poopoo'];
+				break;
+		}
 	}
 }
 
@@ -23,19 +86,39 @@ const abilities: string[] = ['strength', 'dexterity', 'constitution', 'intellige
 const classes: string[] = ['barbarian', 'bard', 'cleric', 'druid', 'fighter', 'monk', 'paladin', 'ranger', 'rogue', 'sorcerer', 'warlock', 'wizard'];
 const races: string[] = ['dragonborn', 'dwarf', 'elf', 'gnome', 'half-elf', 'half-orc', 'halfling', 'human', 'tiefling'];
 const raceInfo: Map<string, RaceInfo> = new Map<string, RaceInfo>([
-	['dragonborn', new RaceInfo(18, 100, 60, 120, 150, 300)],
-	['dwarf', new RaceInfo(18, 100, 60, 120, 150, 300)],
-	['elf', new RaceInfo(18, 100, 60, 120, 150, 300)],
-	['gnome', new RaceInfo(18, 100, 60, 120, 150, 300)],
-	['helf-elf', new RaceInfo(18, 100, 60, 120, 150, 300)],
-	['half-orc', new RaceInfo(18, 100, 60, 120, 150, 300)],
-	['halfling', new RaceInfo(18, 100, 60, 120, 150, 300)],
-	['human', new RaceInfo(18, 100, 60, 120, 150, 300)],
-	['tiefling', new RaceInfo(18, 100, 60, 120, 150, 300)]
+	['dragonborn', new RaceInfo('dragonborn', 15, 80, 78, 90, 215, 300)],
+	['dwarf', new RaceInfo('dwarf', 50, 350, 48, 60, 120, 200)],
+	['elf', new RaceInfo('elf', 100, 750, 54, 78, 125, 215)],
+	['gnome', new RaceInfo('gnome', 40, 480, 32, 48, 40, 45)],
+	['half-elf', new RaceInfo('half-elf', 20, 190, 58, 74, 140, 220)],
+	['half-orc', new RaceInfo('half-orc', 14, 75, 60, 80, 150, 250)],
+	['halfling', new RaceInfo('halfling', 20, 250, 32, 42, 35, 45)],
+	['human', new RaceInfo('human', 18, 80, 60, 78, 130, 225)],
+	['tiefling', new RaceInfo('tiefling', 18, 90, 60, 78, 130, 225)]
 ]);
 
 
 // Utility
+
+// Answer by joshuakcockrell: https://stackoverflow.com/questions/25582882/javascript-math-random-normal-distribution-gaussian-bell-curve
+function getRandomNumberStandardDist(min, max, skew) {
+	let u = 0, v = 0;
+	while (u === 0) u = Math.random() //Converting [0,1) to (0,1)
+	while (v === 0) v = Math.random()
+	let num = Math.sqrt(-2.0 * Math.log(u)) * Math.cos(2.0 * Math.PI * v)
+
+	num = num / 10.0 + 0.5 // Translate to 0 -> 1
+	if (num > 1 || num < 0)
+		num = getRandomNumberStandardDist(min, max, skew) // resample between 0 and 1 if out of range
+
+	else {
+		num = Math.pow(num, skew) // Skew
+		num *= max - min // Stretch to fill range
+		num += min // offset to min
+	}
+	return Math.floor(num);
+}
+
 function randomNumber(min, max) {
 	return Math.floor(Math.random() * (max - min) + min);
 }
@@ -56,8 +139,26 @@ function getHitpoints(hitDie: number, score: number): number {
 	return hitDie + getModifier(score);
 }
 
-function getNpcName(): string {
-	return Math.floor(Math.random() * 2) == 0 ? 'Hector The Well Endowed' : 'Marrrrrr';
+function getHeight(min, max, skew): string {
+	const height = getRandomNumberStandardDist(min, max, skew),
+		feet = Math.floor(height / 12),
+		inches = height % 12;
+
+	return feet + "' " + inches + '"';
+}
+
+function getNpcName(race: string, gender: string): string {
+	// @ts-ignore
+	const rand1 = Math.floor(Math.random() * raceInfo.get(race)!.names[gender.toLowerCase()].length),
+		// @ts-ignore
+		rand2 = Math.floor(Math.random() * raceInfo.get(race)!.names.surname.length);
+
+	// @ts-ignore
+	let last = raceInfo.get(race)!.names.surname[rand2],
+		// @ts-ignore
+		first = raceInfo.get(race)!.names[gender.toLowerCase()][rand1];
+
+	return first + " " + last;
 }
 
 function getModifier(score: number): number {
@@ -87,6 +188,7 @@ export class NpcCard extends Component<any, any> {
 	constructor(props: any) {
 		super(props);
 		this.state = {
+			name: null,
 			race: null,
 			class: null,
 			gender: null,
@@ -117,6 +219,8 @@ export class NpcCard extends Component<any, any> {
 		abilities.forEach((key) => {
 			let score: number = this.generateStat(),
 				hasBonus: boolean = false;
+
+			console.log(key);
 
 			if (bonuses && bonuses.has(key.substring(0, 3))) {
 				//@ts-ignore I check if the key exists above
@@ -161,10 +265,11 @@ export class NpcCard extends Component<any, any> {
 
 		Promise.all([race, npcClass]).then((r) => {
 			this.generateStats(r[1].class.hit_die, r[0].race.ability_bonuses);
+			const gender = getNpcGender();
 
 			this.setState({
-				name: getNpcName(),
-				gender: getNpcGender()
+				name: getNpcName(r[0].race.index, gender),
+				gender: gender
 			});
 
 			setTimeout(() => {
@@ -209,6 +314,9 @@ export class NpcCard extends Component<any, any> {
 		console.log('NPC State', npc);
 		return (
 			<>
+				<select>
+					{[...Array(20)].map((a, index) => <option key={Math.random() + index} value={index + 1}>{index + 1}</option>)}
+				</select>
 				<div className="npc-card" >
 					<div className="npc-card__header">
 						<div>
@@ -227,6 +335,13 @@ export class NpcCard extends Component<any, any> {
 					<div className="npc-card__image" >
 						<img src="https://www.fillmurray.com/300/300" />
 					</div>
+					<div className="npc-card__bio">
+						<ol>
+							<li><strong>Age:&nbsp;</strong>{getRandomNumberStandardDist(raceInfo.get(npc.race.index)!.age.min, raceInfo.get(npc.race.index)!.age.max, 1)} years</li>
+							<li><strong>Height:&nbsp;</strong>{getHeight(raceInfo.get(npc.race.index)!.height.min, raceInfo.get(npc.race.index)!.height.max, 1)}</li>
+							<li><strong>Weight:&nbsp;</strong>{getRandomNumberStandardDist(raceInfo.get(npc.race.index)!.weight.min, raceInfo.get(npc.race.index)!.weight.max, 1)} lbs</li>
+						</ol>
+					</div>
 					<ul className="npc-card__ability-scores" >
 						{this.renderAbilityScore('Strength', npc.abilityScores.strength)}
 						{this.renderAbilityScore('Dexterity', npc.abilityScores.dexterity)}
@@ -235,9 +350,6 @@ export class NpcCard extends Component<any, any> {
 						{this.renderAbilityScore('Wisdom', npc.abilityScores.wisdom)}
 						{this.renderAbilityScore('Charisma', npc.abilityScores.charisma)}
 					</ul>
-					<div className='npc-card__bio'>
-						<p>blah blah blah</p>
-					</div>
 				</div>
 				<button className="generate-npc-button" onClick={this.handleClick}>Generate NPC</button>
 			</>
