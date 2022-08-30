8dbeffd3a464292fcf421694366b5caeefa5e6ae
Josh Delgado <josh.high5@gmail.com>
2022-08-30 11:36:21 -0600

    Added app header and color variables

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './App.scss';
 import { NpcCard } from './components/npc-card/npc-card.tsx';
+import { AppHeader } from './components/app-header/app-header.tsx';
 
 function App() {
 	return (
 		<div className="app">
-			<NpcCard></NpcCard>
+			<AppHeader></AppHeader>
+			<div className="app__content">
+				<NpcCard></NpcCard>
+			</div>
 		</div>
 	);
 }
---
+++ src/components/app-header/app-header.tsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+export class AppHeader extends Component<any, any> {
+	constructor(props: any) {
+		super(props);
+	}
+	render() {
+		return (
+			<header className="app-header">
+				<div className="app-header__content">
+					<h2 className="app-header__title">Roll4d6</h2>
+				</div>
+			</header>
+		);
+	}
+}
