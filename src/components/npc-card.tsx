be4f1bc2db3f0a3895dc4d5dc11ecae05a91cb0f
Josh Delgado <josh.high5@gmail.com>
2022-09-01 09:15:34 -0600

    before eject again

--- src/components/npc-card.tsx
+++ src/components/npc-card.tsx
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import { SimpleRange } from '../interfaces/simple-range';
 import { Stat } from '../interfaces/stat';
 import { Alignment } from '../interfaces/alignment';
+// @ts-ignore
+import { gods } from '../consts/consts.ts';
 
 class RaceInfo {
 	name: string;
@@ -110,17 +112,17 @@ const races: Map<string, RaceInfo> = new Map<string, RaceInfo>([
 	['human', new RaceInfo('Human', 18, 80, 60, 78, 130, 225)],
 	['tiefling', new RaceInfo('Tiefling', 18, 90, 60, 78, 130, 225)]
 ]);
-const gods: Map<string, string[]> = new Map<string, string[]>([
-	['CE', ['Beshaba, goddess of misfortune', 'Cyric, god of lies', 'Malar, god of the hunt', 'Talona, goddess of disease and poison', 'Talos, god of storms', 'Umberlee, goddess of the sea']],
-	['CN', ['Leira, goddess of illusion', 'Mask, god of thieves']],
-	['CG', ['Lliira, goddess of joy', 'Selûne, goddess of the moon', 'Sune, goddess of love and beauty', 'Tymora, goddess of good fortune']],
-	['LE', ['Bane, god of tyranny', 'Loviatar, goddess of pain']],
-	['LN', ['Azuth, god of wizards', 'Helm, god of protection', 'Kelemvor, god of the dead', 'Savras, god of divination and fate']],
-	['LG', ['Ilmater, god of endurance', 'Torm, god of courage and self-sacrifice', 'Tyr, god of justice']],
-	['NE', ['Auril, goddess of winter', 'Bhaal, god of murder', 'Myrkul, god of death', 'Shar, goddess of darkness and loss']],
-	['N', ['Gond, god of craft', 'Oghma, god of knowledge', 'Silvanus, god of wild nature', 'Tempus, god of war', 'Waukeen, goddess of trade']],
-	['NG', ['Chauntea, goddess of agriculture', 'Deneir, god of writing', 'Eldath, goddess of peace', 'Lathander, god of birth and renewal', 'Mielikki, goddess of forests', 'Milil, god of poetry and song', 'Mystra, goddess of magic']]
-]);
+// const gods: Map<string, string[]> = new Map<string, string[]>([
+// 	['CE', ['Beshaba, goddess of misfortune', 'Cyric, god of lies', 'Malar, god of the hunt', 'Talona, goddess of disease and poison', 'Talos, god of storms', 'Umberlee, goddess of the sea']],
+// 	['CN', ['Leira, goddess of illusion', 'Mask, god of thieves']],
+// 	['CG', ['Lliira, goddess of joy', 'Selûne, goddess of the moon', 'Sune, goddess of love and beauty', 'Tymora, goddess of good fortune']],
+// 	['LE', ['Bane, god of tyranny', 'Loviatar, goddess of pain']],
+// 	['LN', ['Azuth, god of wizards', 'Helm, god of protection', 'Kelemvor, god of the dead', 'Savras, god of divination and fate']],
+// 	['LG', ['Ilmater, god of endurance', 'Torm, god of courage and self-sacrifice', 'Tyr, god of justice']],
+// 	['NE', ['Auril, goddess of winter', 'Bhaal, god of murder', 'Myrkul, god of death', 'Shar, goddess of darkness and loss']],
+// 	['N', ['Gond, god of craft', 'Oghma, god of knowledge', 'Silvanus, god of wild nature', 'Tempus, god of war', 'Waukeen, goddess of trade']],
+// 	['NG', ['Chauntea, goddess of agriculture', 'Deneir, god of writing', 'Eldath, goddess of peace', 'Lathander, god of birth and renewal', 'Mielikki, goddess of forests', 'Milil, god of poetry and song', 'Mystra, goddess of magic']]
+// ]);
 
 /*
 *
--- src/components/npc-generator.tsx
+++ src/components/npc-generator.tsx
@@ -1,7 +1,11 @@
 import React, { Component } from 'react';
+// @ts-ignore
 import { AppFooter } from './app-footer.tsx';
+// @ts-ignore
 import { AppHeader } from './app-header.tsx';
+// @ts-ignore
 import { NpcCard } from './npc-card.tsx';
+// @ts-ignore
 import { NpcOptions } from './npc-options.tsx';
 
 export class NpcGenerator extends Component<any, any> {
