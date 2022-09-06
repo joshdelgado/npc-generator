b6660e7015c0bd3643fac2c5145acda9dc95c93e
Josh Delgado <josh.high5@gmail.com>
2022-09-05 20:15:03 -0600

    Polished up the pre footer

--- src/components/pre-footer.tsx
+++ src/components/pre-footer.tsx
@@ -7,8 +7,12 @@ export class PreFooter extends Component<any, any> {
 			<div className="pre-footer">
 				<div className="pre-footer__content">
 					<ul>
-						<li>Send me bitcoin?</li>
-						<li>Check out my <a href="https://www.youtube.com/thejunkdrawershow">D&D Campaign</a></li>
+						<li className="pre-footer__cta">Find this useful? Buy me a drink! <span>(please and thank you)</span></li>
+						<li className="pre-footer__wallets">
+							<img src={`${process.env.PUBLIC_URL}/img/btc-wallet.png`} />
+							<img src={`${process.env.PUBLIC_URL}/img/eth-wallet.png`} />
+						</li>
+						<li className="pre-footer__cta">Check out my <a href="https://www.youtube.com/thejunkdrawershow">D&D Campaign</a> <span>🤙</span></li>
 					</ul>
 				</div>
 			</div>
