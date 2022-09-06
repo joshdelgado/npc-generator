9e3436eaccf6ef8f1f399e918596266c40597752
Josh Delgado <josh.high5@gmail.com>
2022-09-05 19:45:22 -0600

    Updated header styles

--- src/components/app-header.tsx
+++ src/components/app-header.tsx
@@ -12,7 +12,6 @@ export class AppHeader extends Component<any, any> {
 						<div className="logo__die"></div>
 						<div className="logo__die"></div>
 					</div>
-					<h2 className="app-header__title">Roll4d6</h2>
 				</div>
 			</header>
 		);
