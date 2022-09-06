a64182c4c54ed34e63cfc63543158b5e2ea5ab8e
Josh Delgado <josh.high5@gmail.com>
2022-09-06 14:32:58 -0600

    Improved qr code click/tap UX

--- src/components/pre-footer.tsx
+++ src/components/pre-footer.tsx
@@ -2,12 +2,20 @@ import React, { Component } from 'react';
 
 export class PreFooter extends Component<any, any> {
 
-	btcAddress: string = 'bc1q9c4tj9jut566unzys3nn96v28yulr3g0xyau6s';
-	ethAddress: string = '0xbcB7bdb58cB4A8a7D2A16bc5517f60C284c7cF75';
+	state = {
+		alertClicked: false,
+		clickedWallet: null
+	}
 
-	alertCopy = (stringCopied: string) => {
+	private btcAddress: string = 'bc1q9c4tj9jut566unzys3nn96v28yulr3g0xyau6s';
+	private ethAddress: string = '0xbcB7bdb58cB4A8a7D2A16bc5517f60C284c7cF75';
+
+	alertCopy = (walletName: string, stringCopied: string) => {
 		navigator.clipboard.writeText(stringCopied);
-		alert("copied: " + stringCopied);
+		this.setState({ alertClicked: true, clickedWallet: walletName });
+		setTimeout(() => {
+			this.setState({ alertClicked: false, clickedWallet: null });
+		}, 5000);
 	}
 
 	render() {
@@ -17,9 +25,9 @@ export class PreFooter extends Component<any, any> {
 					<ul>
 						<li className="pre-footer__wallets">
 							<p>Find this useful? Buy me a drink! <span>(please and thank you)</span></p>
-							<img className="pre-footer__qr-code" src={`${process.env.PUBLIC_URL}/img/btc-wallet.png`} alt="Bitcoin Wallet Address" title={this.btcAddress} onClick={() => { this.alertCopy(this.btcAddress) }} />
-							<img className="pre-footer__qr-code" src={`${process.env.PUBLIC_URL}/img/eth-wallet.png`} alt="Ethereum Wallet Address" title={this.ethAddress} onClick={() => { this.alertCopy(this.ethAddress) }} />
-							<span>Click or tap to copy</span>
+							<img className="pre-footer__qr-code" src={`${process.env.PUBLIC_URL}/img/btc-wallet.png`} alt="Bitcoin Wallet Address" title={this.btcAddress} onClick={() => { this.alertCopy('Bitcoin', this.btcAddress) }} />
+							<img className="pre-footer__qr-code" src={`${process.env.PUBLIC_URL}/img/eth-wallet.png`} alt="Ethereum Wallet Address" title={this.ethAddress} onClick={() => { this.alertCopy('Ethereum', this.ethAddress) }} />
+							{this.state.alertClicked ? <p>{this.state.clickedWallet} Address Copied!</p> : <span>Click or tap to copy</span>}
 						</li>
 						<li className="pre-footer__cta">Check out my <a href="https://www.youtube.com/thejunkdrawershow">D&D Campaign</a> <span>🤙</span></li>
 					</ul>
