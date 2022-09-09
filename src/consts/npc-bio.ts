4611a3ae97cd6d8d90cf52817d8b58c84e2d0708
Josh Delgado <josh.high5@gmail.com>
2022-09-09 10:04:40 -0600

    Small logo changes, updated readMe, added more npc bio options

--- src/consts/npc-bio.ts
+++ src/consts/npc-bio.ts
@@ -1,19 +1,23 @@
 /*
-*
-* These constants are used to generate the Nc'cs bio
-*
+* These constants are used to generate the Npc's bio
 */
 
 // General adjective to describe personality
-export const adjectives: string[] = ['fiesty', 'stubborn', 'timid', 'sneaky', 'aggressive', 'horny', 'punctual'];
+export const adjectives: string[] = ['feisty', 'stubborn', 'timid', 'sneaky', 'aggressive', 'horny', 'punctual', 'jolly', 'cranky', 'blunt', 'angry', 'reserved', 'quiet', 'soft', 'hungry', 'happy', 'silly', 'suspicious', 'unnecessarily formal', 'shockingly strong', 'lying', 'friendly', 'neurotic', 'helpful'];
 
-// Things they do
+// Things they do/have
 export const traits: string[] = [
 	'who runs their family\'s business.',
 	'who collects toads in their free time.',
 	'with more clothing options than common sense.',
-	'that loves their dog more than anything.',
-	'with a love-hate relationship with cooking.'
+	'who loves their dog more than anything.',
+	'with a love-hate relationship with cooking.',
+	'with a slight tick from a traumatic experience.',
+	'who has a handful of tattoos.',
+	'who is allergic to birds.',
+	'with surprisngly floral smelling hair.',
+	'who refuses to eat vegetables unless covered in cheese.',
+	'with a chip on their shoulder'
 ]
 
 // Ways they behave
@@ -21,16 +25,28 @@ export const traits2: string[] = [
 	'They are a skilled combatant and are proud of it.',
 	'They are not a fan of small talk.',
 	'They look for the good in everyone they meet.',
-	'They manage to find something wrong with everything.'
+	'They manage to find something wrong with everything.',
+	'They are constantly eating some type of seed.',
+	'They tend to hold eye contact just a little too long.',
+	'They rarely make eye contact.',
+	'When they feel uncomfortable, they use big words they don\'t fully understand.',
+	'They are very confident in themselves and it is uplifting.',
+	'Their laugh is contagious.',
 ]
 
 // Modifies how strong their faith is
+// They have a 40% chance of being a basic bitch follower
 export const religiousAdjective: string[] = [
+	'are a',
 	'are a devout',
 	'are a zealous',
 	'pretend to be a',
 	'are a nominal',
-	'are a bregrudging'
+	'are a bregrudging',
+	'are a',
+	'were once a',
+	'are a',
+	'are a',
 ];
 
 // Describes how much wealth they have
@@ -39,12 +55,17 @@ export const socioeconomic: string[] = [
 	'are poor as fuck.',
 	'were born rich but are terrible with money.',
 	'live comfortably.',
-	'though they have little money, they are content.'
+	'though they have little money, they are content.',
+	'live in squalor.',
+	'have no need for money.',
+	'never feel safe that they have enough money.',
+	'hoard money and trinkets like a dragon.',
+	'is in some serious debt.'
 ];
 
 // A quirk they have to give them some el oh el ha ha
 export const quirks: string[] = [
-	'They are an aspiring matress salesman.',
+	'They are an aspiring materess salesman.',
 	'Their favorite thing to do is eat pinecones.',
 	'They believe they are a God stuck in mortal form.',
 	'They don\'t like cheese unless it is frozen solid.',
@@ -55,5 +76,14 @@ export const quirks: string[] = [
 	'They were raised by wolves and are searching for their two brothers, Vin and Paul.',
 	'They are an accomplished boar racer.',
 	'They have a wooden pegleg that is filled with a secret.',
-	'When they hear a special word they instantly fall asleep.'
+	'When they hear a special word they instantly fall asleep.',
+	'They don\'t believe birds are real.',
+	'They are missing a finger that they have replaced with a spoon.',
+	'They are always cold even in the middle of summer.',
+	'They are like... always sweating.',
+	'Sometimes they sing their inside thoughts outloud.'
+];
+
+export const plotHooks: string[] = [
+	''
 ];
