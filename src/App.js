f2467913829543db0a8acaaf9d75bef7d57b113b
Josh Delgado <josh.high5@gmail.com>
2022-09-12 11:34:03 -0600

    Moved tag manager to react module

--- src/App.js
+++ src/App.js
@@ -2,7 +2,13 @@ import './App.scss';
 import { AppHeader } from './components/app-header';
 import { AppFooter } from './components/app-footer';
 import { NpcGenerator } from './components/npc-generator';
+import TagManager from '.react-gtm-module';
 
+const tagManagerArgs = {
+	gtmId: 'G-L9ND5XE2K1'
+}
+
+TagManager.initialize(tagManagerArgs);
 
 function App() {
 	return (
